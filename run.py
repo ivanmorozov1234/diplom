dd6027f762bf0dc5197f3661b083de8469ff0d4d
ivanmorozov1234 <morozovivan05@mail.ru>
2026-05-18 14:39:32 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
-
 from dotenv import load_dotenv
+from app import create_app
 
 load_dotenv()
-
-from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
